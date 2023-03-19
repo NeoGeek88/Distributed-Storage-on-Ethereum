50594356b361bd18883a76526d5e57bc07672417
NeoGeek88 <neogeek88@gmail.com>
2023-03-18 17:53:59 -0700

    change hash type to bytes32

--- Contract/contract.sol
+++ Contract/contract.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.4;
 
 // provide _msgSender() and _msgData
-import "@openzeppelin/contracts/utils/Context.sol";
+import "./Context.sol";
 
 // provide _owner
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Ownable.sol";
 
 // provide MerkleProof
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+// import "./MerkleProof.sol";
 
 contract DS is Context, Ownable {
     
     // File chunk structure, used inside the File structure.
     struct FileChunk{
-        string chunkHash;
+        bytes32 chunkHash;
         string nodeId;
     }
     // File metadata structure.
@@ -22,20 +22,20 @@ contract DS is Context, Ownable {
         address owner;
  		string fileName;
         uint256 fileSize;
-        string rootHash;
+        bytes32 rootHash;
         uint256 fileChunkCount;
         FileChunk[] fileChunks;
     }
     
     // Mapping for file owner to all its files. 
-    mapping(address => string[]) private _fileMapping;
+    mapping(address => bytes32[]) private _fileMapping;
     // Mapping the root hash of a file to its metadata.
-    mapping(string => File) private _fileList;
+    mapping(bytes32 => File) private _fileList;
 
     /**
      * Modifier to determine if the address is the file owner.
      */
-    modifier isFileOwner(string memory _rootHash) {
+    modifier isFileOwner(bytes32 _rootHash) {
         require(_fileList[_rootHash].owner == _msgSender(), "You don't have access to this file");
         _;
     }
@@ -43,13 +43,13 @@ contract DS is Context, Ownable {
     /*
      * Add new file and associate it with the owner via mapping (everyone can execute this function).
      */
-    function addFile(string memory _fileName, uint256 _fileSize, string memory _rootHash, FileChunk[] memory _fileChunks) public returns(bool) {
+    function addFile(string memory _fileName, uint256 _fileSize, bytes32 _rootHash, FileChunk[] memory _fileChunks) public returns(bool) {
         _fileList[_rootHash].owner = _msgSender();
         _fileList[_rootHash].fileName = _fileName;
         _fileList[_rootHash].fileSize = _fileSize;
         _fileList[_rootHash].rootHash = _rootHash;
         _fileList[_rootHash].fileChunkCount = _fileChunks.length;
-        for (uint i=0; i<_fileChunks.length; i++){
+        for (uint256 i=0; i<_fileChunks.length; i++){
             _fileList[_rootHash].fileChunks.push(_fileChunks[i]);
         }
         _fileMapping[_msgSender()].push(_rootHash);
@@ -62,7 +62,7 @@ contract DS is Context, Ownable {
     function listFiles() public view returns(File[] memory) {
         File memory file;
         File[] memory files = new File[](_fileMapping[_msgSender()].length);
-        for (uint i=0; i<_fileMapping[_msgSender()].length; i++) {
+        for (uint256 i=0; i<_fileMapping[_msgSender()].length; i++) {
             file = _fileList[_fileMapping[_msgSender()][i]];
             files[i] = file;
         }
@@ -72,13 +72,12 @@ contract DS is Context, Ownable {
     /*
      * Retrive single file information from owner's file list. (only file owner is allowed to execute this function).
      */
-    function getFile(string memory _rootHash) public view returns(File memory) {
-        require(_fileList[_rootHash].owner == _msgSender(), "You don't have access to this file");
+    function getFile(bytes32 _rootHash) public view isFileOwner(_rootHash) returns(File memory) {
         return _fileList[_rootHash];
     }
 
-    function removeFile(string memory _rootHash) public isFileOwner(_rootHash) returns(bool) {
-        for (uint i = 0; i < _fileMapping[_msgSender()].length; i++) {
+    function removeFile(bytes32 _rootHash) public isFileOwner(_rootHash) returns(bool) {
+        for (uint256 i = 0; i < _fileMapping[_msgSender()].length; i++) {
             bytes32 storageHash = keccak256(abi.encodePacked(_fileMapping[_msgSender()][i]));
             bytes32 memoryHash = keccak256(abi.encodePacked(_rootHash));
             if (storageHash == memoryHash) {
@@ -90,17 +89,17 @@ contract DS is Context, Ownable {
         return true;
     }
 
-    function checkFileList(string memory _rootHash) public view returns (File memory) {
+    function checkFileList(bytes32 _rootHash) public view onlyOwner() returns (File memory) {
         return _fileList[_rootHash];
     }
 
     /*
      * Util function to delete a value at 'index' from an array.
      */
-    function deleteFileMappingByIndex(uint index) private {
+    function deleteFileMappingByIndex(uint256 index) private {
         require(index < _fileMapping[_msgSender()].length, "Index out of bounds");
         
-        for (uint i = index; i < _fileMapping[_msgSender()].length-1; i++) {
+        for (uint256 i = index; i < _fileMapping[_msgSender()].length-1; i++) {
             _fileMapping[_msgSender()][i] = _fileMapping[_msgSender()][i+1];
         }
         
@@ -157,7 +156,7 @@ contract DS is Context, Ownable {
     function listNodes() public view returns(Node[] memory) {
         Node memory node;
         Node[] memory nodes = new Node[](_nodeMapping[_msgSender()].length);
-        for (uint i=0; i<_nodeMapping[_msgSender()].length; i++) {
+        for (uint256 i=0; i<_nodeMapping[_msgSender()].length; i++) {
             node = _nodeList[_nodeMapping[_msgSender()][i]];
             nodes[i] = node;
         }
@@ -167,13 +166,12 @@ contract DS is Context, Ownable {
     /*
      * Retrive single file information from owner's file list. (only file owner is allowed to execute this function).
      */
-    function getNode(string memory _nodeId) public view returns(Node memory) {
-        require(_nodeList[_nodeId].owner == _msgSender(), "You don't have access to this node");
+    function getNode(string memory _nodeId) public view isNodeOwner(_nodeId) returns(Node memory) {
         return _nodeList[_nodeId];
     }
 
     function removeNode(string memory _nodeId) public isNodeOwner(_nodeId) returns(bool) {
-        for (uint i = 0; i < _nodeMapping[_msgSender()].length; i++) {
+        for (uint256 i = 0; i < _nodeMapping[_msgSender()].length; i++) {
             bytes32 storageHash = keccak256(abi.encodePacked(_nodeMapping[_msgSender()][i]));
             bytes32 memoryHash = keccak256(abi.encodePacked(_nodeId));
             if (storageHash == memoryHash) {
@@ -195,7 +193,7 @@ contract DS is Context, Ownable {
     function deleteNodeMappingByIndex(uint index) private {
         require(index < _nodeMapping[_msgSender()].length, "Index out of bounds");
         
-        for (uint i = index; i < _nodeMapping[_msgSender()].length-1; i++) {
+        for (uint256 i = index; i < _nodeMapping[_msgSender()].length-1; i++) {
             _nodeMapping[_msgSender()][i] = _nodeMapping[_msgSender()][i+1];
         }
         
